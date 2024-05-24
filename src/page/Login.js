fdd5c7194fbc22bd3bb79d92dd852ddb7cf7b8ae
komangchandra <komangchandraaa1@gmail.com>
2024-05-24 15:08:51 +0700

    fix: login responsive(2)

--- src/page/Login.js
+++ src/page/Login.js
@@ -51,53 +51,49 @@ class Login extends Component {
   render() {
     return (
       <>
-        <div className="container">
-          {/* Outer Row */}
-          <div className="row justify-content-center">
-            <div className="col-xl-10 col-lg-12 col-md-9">
-              <div className="card o-hidden border-0 shadow-lg my-5">
-                <div className="card-body p-0">
-                  {/* Nested Row within Card Body */}
-                  <div className="row p-5">
-                    <div className="col-sm-8 mx-auto">
-                      <div className="py-2">
-                        <div className="text-center">
-                          <h1 className="h4 text-gray-900 mb-4">
-                            Welcome Back!
-                          </h1>
-                        </div>
-                        <form className="user">
-                          <div className="form-group">
-                            <input
-                              type="email"
-                              className="form-control form-control-user"
-                              id="exampleInputEmail"
-                              aria-describedby="emailHelp"
-                              placeholder="Enter Email Address..."
-                              onChange={(e) => {
-                                this.setState({ email: e.target.value });
-                              }}
-                            />
-                          </div>
-                          <div className="form-group">
-                            <input
-                              type="password"
-                              className="form-control form-control-user"
-                              id="exampleInputPassword"
-                              placeholder="Password"
-                              onChange={(e) => {
-                                this.setState({ password: e.target.value });
-                              }}
-                            />
-                          </div>
-                          <hr />
-                          <button
-                            onClick={this.handleLogin}
-                            className="btn btn-primary btn-user btn-block">
-                            Login
-                          </button>
-                        </form>
+        {/* Outer Row */}
+        <div className="row justify-content-center my-5 py-4">
+          <div className="col-xl-6">
+            <div className="card o-hidden border-0 shadow-lg my-5">
+              <div className="card-body p-0">
+                {/* Nested Row within Card Body */}
+                <div className="row p-5">
+                  <div className="col-sm-8 mx-auto">
+                    <div className="py-2">
+                      <div className="text-center">
+                        <h1 className="h4 text-gray-900 mb-4">Welcome Back!</h1>
                       </div>
+                      <form className="user">
+                        <div className="form-group">
+                          <input
+                            type="email"
+                            className="form-control form-control-user"
+                            id="exampleInputEmail"
+                            aria-describedby="emailHelp"
+                            placeholder="Enter Email Address..."
+                            onChange={(e) => {
+                              this.setState({ email: e.target.value });
+                            }}
+                          />
+                        </div>
+                        <div className="form-group">
+                          <input
+                            type="password"
+                            className="form-control form-control-user"
+                            id="exampleInputPassword"
+                            placeholder="Password"
+                            onChange={(e) => {
+                              this.setState({ password: e.target.value });
+                            }}
+                          />
+                        </div>
+                        <hr />
+                        <button
+                          onClick={this.handleLogin}
+                          className="btn btn-primary btn-user btn-block">
+                          Login
+                        </button>
+                      </form>
                     </div>
                   </div>
                 </div>
